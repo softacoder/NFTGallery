a69586bd581f9682eb1e22c9902f90c88c212d8e
softacoder <113185323+softacoder@users.noreply.github.com>
2024-03-10 13:55:48 +0000

    commit nftCard.jsx

--- nftgallery/nftgallery/pages/index.jsx
+++ nftgallery/nftgallery/pages/index.jsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 import { useState } from 'react'
 import { NFTCard } from './components/nftCard'
 
-// 55: 20
-
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
   const [collection, setCollectionAddress] = useState("");
@@ -22,7 +20,7 @@ const Home = () => {
       var requestOptions = {
         method: "GET",
       };
-
+    
       
       nfts = await fetch(fetchURL, requestOptions).then(data => data.json);
     } else {
@@ -48,16 +46,15 @@ const Home = () => {
     const nfts = await fetch(fetchURL, requestOptions).then(data => data.json());
     if (nfts) {
       console.log("NFTs in collection", nfts);
-      setNFTs(nfts.nfts)
+      setNFTs(nfts.nfts) 
     }
   }
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <div>
-        <input
-          onChange={(e) => {
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+    <div className="flex flex-col w-full justify-center gap-y-2">
+      <input disabled={!fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg test-grey-800 focus:outline-blue-300 disabled:bg-slate-50 disabled: text-grey-50" onChange={(e) => {
             setWalletAddress(e.target.value);
           }}
           value={wallet}
@@ -72,11 +69,10 @@ const Home = () => {
           type={"text"}
           placeholder="Add the collection address"
         ></input>
-        <label>
-          <input onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"}></input>Fetch for collection
+        <label className="text-grey-600">
+          <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg test-grey-800 focus:outline-blue-300 disabled:bg-slate-50 disabled: text-grey-50" onChange={(e)=>{setFetchForCollection(e.target.checked)}} type={"checkbox"} className="mr-2"></input>Fetch for collection
         </label>
-        <button
-          onClick={() => {
+        <button className="disabled:bg-slate-500 text-white bg-slate-400 px-4 py-2 mt-3 rounded-sm w1/5"onClick={() => {
             if(fetchForCollection){
             fetchNFTsForCollection()
             }else fetchNFTs()
@@ -85,7 +81,7 @@ const Home = () => {
           Let's go!
         </button>
       </div>
-      <div>
+      <div className="flex flex-wrap gap-y-12 mt-4 v-5/6 gap--x-2 justify-center">
         {
           NFTs.length && NFTs.map(nft => {
             return [
@@ -96,6 +92,6 @@ const Home = () => {
       </div>
     </div>
   );
-};
+      }
 
 export default Home;
